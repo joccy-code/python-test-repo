3e0c4b7d4d90e9d2d2ba807f0f0895a1e7402bf4
Jossy Mesfin Yoseph <jmesfin@ju2.edu.et>
2026-03-04 21:58:03 -0500

    thi is initial commit

---
+++ q1.py
@@ -0,0 +1,6 @@
+#   1)	Build a program that:
+# •	Displays a list of snacks and drinks with item numbers and prices.
+# •	Ask the user to choose items by number in a loop.
+# •	 Keeps track of selected items and their prices.
+# •	Ends when the user types "done".
+# •	Finally prints a receipt showing: List of selected items with prices and total cost
